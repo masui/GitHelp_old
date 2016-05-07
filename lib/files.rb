5cf315e36838bc1db9b16b777194d1a0a78059af
Toshiyuki Masui <masui@pitecan.com>
2016-05-07 23:23:05 +0900

    ...

--- lib/files.rb
+++ lib/files.rb
@@ -7,8 +7,9 @@
 # git ls-files で取得すべきか?
 
 def files
-  files = Dir.glob("*").find_all { |file|
-    file !~ /~$/ &&
-      file !~ /^\#/
-  }
+  # files = Dir.glob("*").find_all { |file|
+  #   file !~ /~$/ &&
+  #     file !~ /^\#/
+  # }
+  files = `git ls-files`.split
 end
