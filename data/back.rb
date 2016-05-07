d8e3222679c8f6c3291abdc18888327308891f2c
Toshiyuki Masui <masui@pitecan.com>
2016-05-08 08:45:58 +0900

    since => until

--- data/back.rb
+++ data/back.rb
@@ -6,14 +6,14 @@
   ],
   [
     "(1|2|3|4|5|6|7|8|9|10|11|12)分前の状態に戻す",
-    'git log --since="#{$1} minutes ago" --oneline | tail -1 | awk \'{print $1}\' | xargs -J xxx git checkout xxx'
+    'git log --until="#{$1} minutes ago" --oneline | head -1 | awk \'{print $1}\' | xargs -J xxx git checkout xxx'
   ],
   [
     "(1|2|3|4|5|6|7|8|9|10|11|12)時間前の状態に戻す",
-    'git log --since="#{$1} hours ago" --oneline | tail -1 | awk \'{print $1}\' | xargs -J xxx git checkout xxx'
+    'git log --until="#{$1} hours ago" --oneline | head -1 | awk \'{print $1}\' | xargs -J xxx git checkout xxx'
   ],
   [
     "(1|2|3|4|5|6|7|8|9|10|11|12)日前の状態に戻す",
-    'git log --since="#{$1} days ago" --oneline | tail -1 | awk \'{print $1}\' | xargs -J xxx git checkout xxx'
+    'git log --until="#{$1} days ago" --oneline | head -1 | awk \'{print $1}\' | xargs -J xxx git checkout xxx'
   ]
 ]
