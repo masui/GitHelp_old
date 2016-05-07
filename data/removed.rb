7a5e634274c1d7c54b828eb16f4b8d0bcfc1952a
Toshiyuki Masui <masui@pitecan.com>
2016-05-07 22:53:07 +0900

    ...

--- data/removed.rb
+++ data/removed.rb
@@ -1,12 +1,16 @@
 # coding: utf-8
-[
+if args.length > 0 && files.length > 0
   [
-    "(#{files.join('|')})というファイルから(#{args.join('|')})という名前が消えたのはいつ？",
-    'git log --oneline | ruby -lane \'puts $_.split(/ /).first\' | xargs -J xxx git show xxx #{$1} | grep -C10 \'^-.*#{$2}\''
-  ],
-  [
-    "(#{args.join('|')})という名前が(#{files.join('|')})というファイルから消えたのはいつ？",
-    'git log --oneline | ruby -lane \'puts $_.split(/ /).first\' | xargs -J xxx git show xxx #{$2} | grep -C10 \'^-.*#{$1}\''
+    [
+      "(#{files.join('|')})というファイルから(#{args.join('|')})という名前が消えたのはいつ？",
+      'git log --oneline | ruby -lane \'puts $_.split(/ /).first\' | xargs -J xxx git show xxx #{$1} | grep -C10 \'^-.*#{$2}\''
+    ],
+    [
+      "(#{args.join('|')})という名前が(#{files.join('|')})というファイルから消えたのはいつ？",
+      'git log --oneline | ruby -lane \'puts $_.split(/ /).first\' | xargs -J xxx git show xxx #{$2} | grep -C10 \'^-.*#{$1}\''
+    ]
   ]
-]
+else
+  nil
+end
 
