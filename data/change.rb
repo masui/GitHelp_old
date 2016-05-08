35151939e532ce61dca1a486971cc09a20cb1cbe
Toshiyuki Masui <masui@pitecan.com>
2016-05-08 09:05:08 +0900

    ブランチ処理追加

--- data/change.rb
+++ data/change.rb
@@ -1,6 +1,10 @@
 # coding: utf-8
 [
   [
+    "(#{files.join('|')})というファイルの((1|2|3|4|5|6|7|8|9)?(0|1|2|3|4|5|6|7|8|9))分前からの変化を(表示する|見る)",
+    'git log --until="#{$2} minutes ago" --oneline | head -1 | awk \'{print $1}\' | xargs -J xxx git diff xxx #{$1}'
+  ],
+  [
     "(#{files.join('|')})というファイルの(1|2|3|4|5)日前からの変化を(表示する|見る)",
     'git log --since="#{$2} days ago" --oneline | tail -1 | awk \'{print $1}\' | xargs -J xxx git diff xxx #{$1}'
   ]
