aa8cf1cb3bf806972af3cc31572aef92b3f7989a
Toshiyuki Masui <masui@pitecan.com>
2016-05-08 12:30:59 +0900

    ひとつ前のコミット/バージョンとの比較

--- data/change.rb
+++ data/change.rb
@@ -39,6 +39,17 @@
     'git log --until="#{$1} days ago" --oneline | head -1 | awk \'{print $1}\' | xargs -J xxx git diff xxx #{$2}'
   ],
 
+  #######
+  [
+    "(#{files.join('|')})というファイルをひとつ前のコミットと比較する",
+    'git diff HEAD^ #{$1}'
+  ],
+  [
+    "(#{files.join('|')})というファイルをひとつ前のバージョンと比較する",
+    'git log --oneline #{$1} | head -2 | tail -1 | awk \'{print $1}\' | xargs -J xxx git diff xxx #{$1}'
+  ],
+
+  #######
   [
     "(1|2|3|4|5|6|7|8|9|10|20|30|40|50)分前から(変化した|変わった)ファイルをリストする",
     'bin/changed \'#{$1} minutes ago\''
