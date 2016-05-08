c0ef99d42bfb958eb54175acff38bf8bd1d4494e
Toshiyuki Masui <masui@pitecan.com>
2016-05-08 12:13:10 +0900

    ...

--- data/change.rb
+++ data/change.rb
@@ -1,6 +1,10 @@
 # coding: utf-8
 [
   [
+    "(#{files.join('|')})というファイルを(1|2|3|4|5|6|7|8|9|10|20|30|40|50)分前のものと比較する",
+    'git log --until="#{$2} minutes ago" --oneline | head -1 | awk \'{print $1}\' | xargs -J xxx git diff xxx #{$1}'
+  ],
+  [
     "(#{files.join('|')})というファイルが(1|2|3|4|5|6|7|8|9|10|20|30|40|50)分前から(変化した|変わった)ところを(表示する|見る)",
     'git log --until="#{$2} minutes ago" --oneline | head -1 | awk \'{print $1}\' | xargs -J xxx git diff xxx #{$1}'
   ],
@@ -8,14 +12,33 @@
     "(1|2|3|4|5|6|7|8|9|10|20|30|40|50)分前から(#{files.join('|')})というファイル(の変化|が変わったところ)を(表示する|見る)",
     'git log --until="#{$1} minutes ago" --oneline | head -1 | awk \'{print $1}\' | xargs -J xxx git diff xxx #{$2}'
   ],
+
+  [
+    "(#{files.join('|')})というファイルを(1|2|3|4|5|6|7)時間前のものと比較する",
+    'git log --until="#{$2} hours ago" --oneline | head -1 | awk \'{print $1}\' | xargs -J xxx git diff xxx #{$1}'
+  ],
+  [
+    "(#{files.join('|')})というファイルが(1|2|3|4|5|6|7)時間前から(変化した|変わった)ところを(表示する|見る)",
+    'git log --until="#{$2} hours ago" --oneline | head -1 | awk \'{print $1}\' | xargs -J xxx git diff xxx #{$1}'
+  ],
+  [
+    "(1|2|3|4|5)時間前から(#{files.join('|')})というファイルが(変化した|変わった)ところを(表示する|見る)",
+    'git log --until="#{$1} hours ago" --oneline | head -1 | awk \'{print $1}\' | xargs -J xxx git diff xxx #{$2}'
+  ],
+
+  [
+    "(#{files.join('|')})というファイルを(1|2|3|4|5)日前のものと比較する",
+    'git log --until="#{$2} days ago" --oneline | head -1 | awk \'{print $1}\' | xargs -J xxx git diff xxx #{$1}'
+  ],
   [
     "(#{files.join('|')})というファイルが(1|2|3|4|5)日前から(変化した|変わった)ところを(表示する|見る)",
-    'git log --since="#{$2} days ago" --oneline | tail -1 | awk \'{print $1}\' | xargs -J xxx git diff xxx #{$1}'
+    'git log --until="#{$2} days ago" --oneline | head -1 | awk \'{print $1}\' | xargs -J xxx git diff xxx #{$1}'
   ],
   [
     "(1|2|3|4|5)日前から(#{files.join('|')})というファイルが(変化した|変わった)ところを(表示する|見る)",
-    'git log --since="#{$1} days ago" --oneline | tail -1 | awk \'{print $1}\' | xargs -J xxx git diff xxx #{$2}'
+    'git log --until="#{$1} days ago" --oneline | head -1 | awk \'{print $1}\' | xargs -J xxx git diff xxx #{$2}'
   ],
+
   [
     "(1|2|3|4|5|6|7|8|9|10|20|30|40|50)分前から(変化した|変わった)ファイルをリストする",
     'bin/changed \'#{$1} minutes ago\''
