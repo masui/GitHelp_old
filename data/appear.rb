93a79e4878037f449d8a5a9f42e13e97bf3059ce
Toshiyuki Masui <masui@pitecan.com>
2016-05-10 06:36:40 +0900

    ...

--- data/appear.rb
+++ data/appear.rb
@@ -2,11 +2,11 @@
 if params.length > 0 && files.length > 0
   [
     [
-      "「(#{files.join('|')})」ファイルに(#{params.join('|')})という名前が出現したのはいつ？",
+      "「(#{files.join('|')})」ファイルに「(#{params.join('|')})」という名前が出現したのはいつ？",
       'git blame #{$1} | grep #{$2}'
     ],
     [
-      "(#{params.join('|')})という名前が「(#{files.join('|')})」ファイルに出現したのはいつ？",
+      "「(#{params.join('|')})」という名前が「(#{files.join('|')})」ファイルに出現したのはいつ？",
       'git blame #{$2} | grep #{$1}'
     ]
   ]
--- data/removed.rb
+++ data/removed.rb
@@ -3,11 +3,11 @@ if params.length > 0 && files.length > 0
 
   [
     [
-      "「(#{files.join('|')})」ファイルから(#{params.join('|')})という名前が消えたのはいつ？",
+      "「(#{files.join('|')})」ファイルから「(#{params.join('|')})」という名前が消えたのはいつ？",
       'git log --oneline | awk \'{print $1}\' | xargs -J xxx git show xxx #{$1} | grep -C10 \'^-.*#{$2}\''
     ],
     [
-      "(#{params.join('|')})という名前が「(#{files.join('|')})」ファイルから消えたのはいつ？",
+      "「(#{params.join('|')})」という名前が「(#{files.join('|')})」ファイルから消えたのはいつ？",
       'git log --oneline | awk \'{print $1}\' | xargs -J xxx git show xxx #{$2} | grep -C10 \'^-.*#{$1}\''
     ]
   ]
