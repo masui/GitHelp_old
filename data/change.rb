dc1598d43e4b28fa28d9b046e89770957c3afb8f
Toshiyuki Masui <masui@pitecan.com>
2016-05-10 11:06:09 +0900

    パタン指定してないときはリストに出さない #5

--- data/change.rb
+++ data/change.rb
@@ -1,92 +1,91 @@
 # coding: utf-8
 
-[
+patterns != ['##PATTERN##'] ?
   [
-    "「(#{files.join('|')})」ファイルを(#{numbers.join('|')})分前のものと比較する",
-    'git log --until="#{$2} minutes ago" --oneline | head -1 | awk \'{print $1}\' | xargs -J xxx git diff xxx #{$1}'
-  ],
-  [
-    "「(#{files.join('|')})」ファイルが(#{numbers.join('|')})分前から(変化した|変わった)ところを(表示する|見る)",
-    'git log --until="#{$2} minutes ago" --oneline | head -1 | awk \'{print $1}\' | xargs -J xxx git diff xxx #{$1}'
-  ],
-  [
-    "「(#{files.join('|')})」ファイルを(#{numbers.join('|')})時間前のものと比較する",
-    'git log --until="#{$2} hours ago" --oneline | head -1 | awk \'{print $1}\' | xargs -J xxx git diff xxx #{$1}'
-  ],
-  [
-    "「(#{files.join('|')})」ファイルが(#{numbers.join('|')})時間前から(変化した|変わった)ところを(表示する|見る)",
-    'git log --until="#{$2} hours ago" --oneline | head -1 | awk \'{print $1}\' | xargs -J xxx git diff xxx #{$1}'
-  ],
-  [
-    "「(#{files.join('|')})」ファイルを(#{numbers.join('|')})日前のものと比較する",
-    'git log --until="#{$2} days ago" --oneline | head -1 | awk \'{print $1}\' | xargs -J xxx git diff xxx #{$1}'
-  ],
-  [
-    "「(#{files.join('|')})」ファイルが(#{numbers.join('|')})日前から(変化した|変わった)ところを(表示する|見る)",
-    'git log --until="#{$2} days ago" --oneline | head -1 | awk \'{print $1}\' | xargs -J xxx git diff xxx #{$1}'
-  ],
-  [
-    "(#{numbers.join('|')})分前の「(#{files.join('|')})」ファイルと現在のものを比較する",
-    'git log --until="#{$1} minutes ago" --oneline | head -1 | awk \'{print $1}\' | xargs -J xxx git diff xxx #{$2}'
-  ],
-  [
-    "(#{numbers.join('|')})時間前の「(#{files.join('|')})」ファイルと現在のものを比較する",
-    'git log --until="#{$1} hours ago" --oneline | head -1 | awk \'{print $1}\' | xargs -J xxx git diff xxx #{$2}'
-  ],
-  [
-    "(#{numbers.join('|')})日前の「(#{files.join('|')})」ファイルと現在のものを比較する",
-    'git log --until="#{$1} days ago" --oneline | head -1 | awk \'{print $1}\' | xargs -J xxx git diff xxx #{$2}'
-  ],
-  [
-    "(#{numbers.join('|')})分前からの「(#{files.join('|')})」ファイル(の変化|が変わったところ)を(表示する|見る)",
-    'git log --until="#{$1} minutes ago" --oneline | head -1 | awk \'{print $1}\' | xargs -J xxx git diff xxx #{$2}'
-  ],
-  [
-    "(#{numbers.join('|')})時間前からの「(#{files.join('|')})」ファイル(の変化|が変わったところ)を(表示する|見る)",
-    'git log --until="#{$1} hours ago" --oneline | head -1 | awk \'{print $1}\' | xargs -J xxx git diff xxx #{$2}'
-  ],
-  [
-    "(#{numbers.join('|')})日前からの「(#{files.join('|')})」ファイル(の変化|が変わったところ)を(表示する|見る)",
-    'git log --until="#{$1} days ago" --oneline | head -1 | awk \'{print $1}\' | xargs -J xxx git diff xxx #{$2}'
-  ],
-
-  #######
-  [
-    "「(#{files.join('|')})」ファイルをひとつ前のコミットと比較する",
-    'git diff HEAD^ #{$1}'
-  ],
-  [
-    "「(#{files.join('|')})」ファイルをふたつ前のコミットと比較する",
-    'git diff HEAD^^ #{$1}'
-  ],
-  [
-    "「(#{files.join('|')})」ファイルをひとつ前のバージョンと比較する",
-    'git log --oneline #{$1} | head -2 | tail -1 | awk \'{print $1}\' | xargs -J xxx git diff xxx #{$1}'
-  ],
-
-  #######
-  [
-    "(#{numbers.join('|')})分前から(変化した|変わった|変更された)ファイルをリストする",
-    'git-changed \'#{$1} minutes ago\''
-  ],
-  [
-    "(#{numbers.join('|')})時間前から(変化した|変わった|変更された)ファイルをリストする",
-    'git-changed \'#{$1} hours ago\''
-  ],
-  [
-    "(#{numbers.join('|')})日前から(変化した|変わった|変更された)ファイルをリストする",
-    'git-changed \'#{$1} days ago\''
-  ],
-  [
-    "(#{numbers.join('|')})分前から(変化してない|変わってない|変わらない|変更されてない)ファイルをリストする",
-    'git-unchanged \'#{$1} minutes ago\''
-  ],
-  [
-    "(#{numbers.join('|')})時間前から(変化してない|変わってない|変わらない|変更されてない)ファイルをリストする",
-    'git-unchanged \'#{$1} hours ago\''
-  ],
-  [
-    "(#{numbers.join('|')})日前から(変化してない|変わってない|変わらない|変更されてない)ファイルをリストする",
-    'git-unchanged \'#{$1} days ago\''
-  ],
-]
+    [
+      "「(#{files.join('|')})」ファイルを(#{numbers.join('|')})分前の(もの|バージョン)と比較する",
+      'git log --until="#{$2} minutes ago" --oneline | head -1 | awk \'{print $1}\' | xargs -J xxx git diff xxx #{$1}'
+    ],
+    [
+      "「(#{files.join('|')})」ファイルが(#{numbers.join('|')})分前から(変化した|変わった)ところを(表示する|見る)",
+      'git log --until="#{$2} minutes ago" --oneline | head -1 | awk \'{print $1}\' | xargs -J xxx git diff xxx #{$1}'
+    ],
+    [
+      "「(#{files.join('|')})」ファイルを(#{numbers.join('|')})時間前の(もの|バージョン)と比較する",
+      'git log --until="#{$2} hours ago" --oneline | head -1 | awk \'{print $1}\' | xargs -J xxx git diff xxx #{$1}'
+    ],
+    [
+      "「(#{files.join('|')})」ファイルが(#{numbers.join('|')})時間前から(変化した|変わった)ところを(表示する|見る)",
+      'git log --until="#{$2} hours ago" --oneline | head -1 | awk \'{print $1}\' | xargs -J xxx git diff xxx #{$1}'
+    ],
+    [
+      "「(#{files.join('|')})」ファイルを(#{numbers.join('|')})日前の(もの|バージョン)と比較する",
+      'git log --until="#{$2} days ago" --oneline | head -1 | awk \'{print $1}\' | xargs -J xxx git diff xxx #{$1}'
+    ],
+    [
+      "「(#{files.join('|')})」ファイルが(#{numbers.join('|')})日前から(変化した|変わった)ところを(表示する|見る)",
+      'git log --until="#{$2} days ago" --oneline | head -1 | awk \'{print $1}\' | xargs -J xxx git diff xxx #{$1}'
+    ],
+    [
+      "(#{numbers.join('|')})分前の「(#{files.join('|')})」ファイルと現在の(もの|バージョン)を比較する",
+      'git log --until="#{$1} minutes ago" --oneline | head -1 | awk \'{print $1}\' | xargs -J xxx git diff xxx #{$2}'
+    ],
+    [
+      "(#{numbers.join('|')})時間前の「(#{files.join('|')})」ファイルと現在の(もの|バージョン)を比較する",
+      'git log --until="#{$1} hours ago" --oneline | head -1 | awk \'{print $1}\' | xargs -J xxx git diff xxx #{$2}'
+    ],
+    [
+      "(#{numbers.join('|')})日前の「(#{files.join('|')})」ファイルと現在の(もの|バージョン)を比較する",
+      'git log --until="#{$1} days ago" --oneline | head -1 | awk \'{print $1}\' | xargs -J xxx git diff xxx #{$2}'
+    ],
+    [
+      "(#{numbers.join('|')})分前からの「(#{files.join('|')})」ファイル(の変化|が変わったところ)を(表示する|見る)",
+      'git log --until="#{$1} minutes ago" --oneline | head -1 | awk \'{print $1}\' | xargs -J xxx git diff xxx #{$2}'
+    ],
+    [
+      "(#{numbers.join('|')})時間前からの「(#{files.join('|')})」ファイル(の変化|が変わったところ)を(表示する|見る)",
+      'git log --until="#{$1} hours ago" --oneline | head -1 | awk \'{print $1}\' | xargs -J xxx git diff xxx #{$2}'
+    ],
+    [
+      "(#{numbers.join('|')})日前からの「(#{files.join('|')})」ファイル(の変化|が変わったところ)を(表示する|見る)",
+      'git log --until="#{$1} days ago" --oneline | head -1 | awk \'{print $1}\' | xargs -J xxx git diff xxx #{$2}'
+    ],
+    [
+      "「(#{files.join('|')})」ファイルをひとつ前のコミットと比較する",
+      'git diff HEAD^ #{$1}'
+    ],
+    [
+      "「(#{files.join('|')})」ファイルをふたつ前のコミットと比較する",
+      'git diff HEAD^^ #{$1}'
+    ],
+    [
+      "「(#{files.join('|')})」ファイルをひとつ前のバージョンと比較する",
+      'git log --oneline #{$1} | head -2 | tail -1 | awk \'{print $1}\' | xargs -J xxx git diff xxx #{$1}'
+    ],
+  ] : [] +
+      [
+        [
+          "(#{numbers.join('|')})分前から(変化した|変わった|変更された)ファイルをリストする",
+          'git-changed \'#{$1} minutes ago\''
+        ],
+        [
+          "(#{numbers.join('|')})時間前から(変化した|変わった|変更された)ファイルをリストする",
+          'git-changed \'#{$1} hours ago\''
+        ],
+        [
+          "(#{numbers.join('|')})日前から(変化した|変わった|変更された)ファイルをリストする",
+          'git-changed \'#{$1} days ago\''
+        ],
+        [
+          "(#{numbers.join('|')})分前から(変化してない|変わってない|変わらない|変更されてない)ファイルをリストする",
+          'git-unchanged \'#{$1} minutes ago\''
+        ],
+        [
+          "(#{numbers.join('|')})時間前から(変化してない|変わってない|変わらない|変更されてない)ファイルをリストする",
+          'git-unchanged \'#{$1} hours ago\''
+        ],
+        [
+          "(#{numbers.join('|')})日前から(変化してない|変わってない|変わらない|変更されてない)ファイルをリストする",
+          'git-unchanged \'#{$1} days ago\''
+        ],
+      ]
