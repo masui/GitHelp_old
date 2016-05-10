c4357b18d53de798a2ee2744d6b545f1e143e5cc
Toshiyuki Masui <masui@pitecan.com>
2016-05-10 18:40:53 +0900

    ignore追加

---
+++ data/ignore.rb
@@ -0,0 +1,11 @@
+# coding: utf-8
+[
+  [
+    "(Gitで?)管理されていないファイルを(表示する|見る|リストする)",
+    'git ls-files --exclude-standard --others --ignored'
+  ],
+  [
+    "(Gitで?)管理されていないファイルのリストを(表示する|見る)",
+    'git ls-files --exclude-standard --others --ignored'
+  ],
+]
