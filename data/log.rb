a81aeb251f3e06feb96f8269880aaba59357fdb4
Toshiyuki Masui <masui@pitecan.com>
2016-05-11 10:25:41 +0900

    ファイル追加のときのログ取得する方法を変更

--- data/log.rb
+++ data/log.rb
@@ -1,7 +1,8 @@
 # coding: utf-8
 [
   [
-    "(#{files.join('|')})が追加されたコミットの情報を表示する",
-    'git-fileadded #{$1} range | xargs git log'
+    "(#{files.join('|')})が追加されたコミットの(情報|ログ)を表示する",
+    'git log --format="%h" #{$1} | tail -1 | xargs -J xxxx git log xxxx #{$1}'
+    # 'git-fileadded #{$1} range | xargs git log' ログの範囲は abcde^..abcde みたいに指定する必要があるがこれを生成するのが難しい!
   ],
 ]
